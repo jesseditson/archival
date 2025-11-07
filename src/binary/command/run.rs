38a30249224024eaac686e4bc275696ccab15f49
Jesse Ditson <jesse.ditson@gmail.com>
2025-11-06 18:34:55 -0800

    add a nicer interactive mode to archival when it is run as a tty

--- src/binary/command/run.rs
+++ src/binary/command/run.rs
@@ -2,6 +2,9 @@ use super::BinaryCommand;
 use crate::FieldConfig;
 use crate::{file_system::WatchableFileSystemAPI, file_system_stdlib, server, site::Site};
 use clap::{arg, value_parser, ArgMatches};
+use console::{style, Term};
+use indicatif::ProgressBar;
+use rsa::pkcs8::der::Writer;
 use std::sync::mpsc;
 use std::time::{Duration, Instant};
 use std::{
@@ -13,7 +16,7 @@ use std::{
     },
     thread,
 };
-use tracing::{info, warn};
+use tracing::warn;
 
 pub struct Command {}
 impl BinaryCommand for Command {
@@ -38,6 +41,8 @@ impl BinaryCommand for Command {
         args: &ArgMatches,
         quit: Arc<AtomicBool>,
     ) -> Result<crate::binary::ExitStatus, Box<dyn std::error::Error>> {
+        let mut term = Term::stdout();
+        let is_interactive = term.features().is_attended();
         let mut fs = file_system_stdlib::NativeFileSystem::new(build_dir);
         let site = Site::load(
             &fs,
@@ -45,10 +50,8 @@ impl BinaryCommand for Command {
         )?;
         FieldConfig::set_global(site.get_field_config(None)?);
         site.sync_static_files(&mut fs)?;
-        if let Err(e) = site.build(&mut fs) {
-            println!("Initial build failed: {}", e);
-        }
-        println!("Watching site: {}", &site);
+        let initial_build = site.build(&mut fs);
+        let mut init_message = format!("Watching site: {}", &site);
         let (tx, rx) = mpsc::channel();
         // This won't leak because the process is ended when we
         // abort anyway
@@ -56,7 +59,6 @@ impl BinaryCommand for Command {
             fs.root.to_owned(),
             site.manifest.watched_paths(),
             move |paths| {
-                info!("changed: {:?}", paths);
                 for path in paths {
                     if let Err(e) = tx.send(path) {
                         warn!("Failed sending change event: {}", e);
@@ -71,9 +73,9 @@ impl BinaryCommand for Command {
                 sb.port(*port);
             }
             let server = sb.build();
-            println!("Serving {}", path.display());
-            println!("See http://{}", server.addr());
-            println!("Hit CTRL-C to stop");
+            init_message += &format!("Serving {}\n", path.display());
+            init_message += &format!("See http://{}\n", server.addr());
+            init_message += "Hit CTRL-C to stop\n";
             thread::spawn(move || {
                 server.serve().unwrap();
             });
@@ -85,6 +87,14 @@ impl BinaryCommand for Command {
         })?;
         let mut last_build = Instant::now();
         let mut changed = false;
+        if is_interactive {
+            term.clear_screen()?;
+        }
+        term.write(init_message.as_bytes())?;
+        if let Err(e) = initial_build {
+            let bar = ProgressBar::new_spinner();
+            bar.finish_with_message(format!("Initial build failed: {}", e));
+        }
         loop {
             match rx.try_recv() {
                 Ok(path) => {
@@ -104,13 +114,32 @@ impl BinaryCommand for Command {
             if changed && Instant::now() - last_build > Duration::from_millis(200) {
                 last_build = Instant::now();
                 changed = false;
-                println!("Rebuilding");
+                let bar = if is_interactive {
+                    let bar = ProgressBar::new_spinner();
+                    bar.enable_steady_tick(Duration::from_millis(100));
+                    term.clear_screen()?;
+                    term.write(init_message.as_bytes())?;
+                    bar.set_message("Rebuilding...");
+                    Some(bar)
+                } else {
+                    println!("Rebuilding...");
+                    None
+                };
                 let mut fs = file_system_stdlib::NativeFileSystem::new(build_dir);
                 site.sync_static_files(&mut fs).unwrap();
-                if let Err(e) = site.build(&mut fs) {
-                    println!("Build failed: {}", e);
+                let output = if let Err(e) = site.build(&mut fs) {
+                    format!("{} {}", style("Build failed:").red(), style(e).red())
                 } else {
-                    println!("Rebuilt in {:?}", Instant::now() - last_build);
+                    format!(
+                        "{} {:?}",
+                        style("Rebuilt in").green(),
+                        style(Instant::now() - last_build).green()
+                    )
+                };
+                if let Some(bar) = bar {
+                    bar.finish_with_message(output);
+                } else {
+                    println!("{}", output);
                 }
             }
         }
