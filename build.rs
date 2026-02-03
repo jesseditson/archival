40d2fc222619936cdcc852e47da1cdb9490c163f
Jesse Ditson <jesse.ditson@gmail.com>
2026-02-02 18:18:08 -0800

    breaking: update archival proto layout and names to pass buf linter compatibility

--- build.rs
+++ build.rs
@@ -5,7 +5,7 @@ fn main() -> Result<()> {
         let mut config = prost_build::Config::new();
         // Derive serde for all generated types so we can convert via serde JSON.
         config.type_attribute(".", "#[derive(serde::Serialize, serde::Deserialize)]");
-        config.compile_protos(&["proto/archival.proto"], &["proto"])?;
+        config.compile_protos(&["proto/archival/v1/archival.proto"], &["proto"])?;
     }
     Ok(())
 }
